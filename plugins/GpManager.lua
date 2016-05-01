294b5dccdb562be7a13d1c965c481c25d801c8c6
MasterKia <mysignup27@gmail.com>
2016-05-01 12:54:06 +0430

    Update and rename TeleIran.lua to GpManager.lua

---
+++ plugins/GpManager.lua
@@ -0,0 +1,19 @@
+do
+function run(msg, matches)
+  return "🔵Group Manager
+
+An advanced group manager !
+@MrTorento [Developer]
+@Alfroshotak [Sudo]
+
+@GpManager"
+  end
+return {
+  description = " TeleIran ", 
+  usage = " TeleIran ",
+  patterns = {
+    "^[Gg][Pp][Mm][Aa][Nn][Aa][Gg][Ee][Rr]$",
+  },
+  run = run
+}
+end
